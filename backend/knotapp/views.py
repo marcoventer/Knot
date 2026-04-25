0fd574964bc6d34f0fb7980b976d895583e9cc26
Marco Venter <marco.c.venter@gmail.com>
2026-04-25 02:10:15 +0200

    added a few urls to see the db on the backend

--- backend/knotapp/views.py
+++ backend/knotapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Post
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,5 +10,20 @@ def home(request):
 
 
 @api_view(["GET"])
-def hello_world(request):
-    return Response({"message": "Hello from Django REST Framework!"})
+def users_list(request):
+    users = User.objects.values("id", "username", "email", "first_name", "last_name")
+    return Response(list(users))
+
+
+@api_view(["GET"])
+def posts_list(request):
+    posts = Post.objects.values(
+        "id",
+        "author_id",
+        "author__username",
+        "content",
+        "category",
+        "is_misleading",
+        "created_at",
+    )
+    return Response(list(posts))
